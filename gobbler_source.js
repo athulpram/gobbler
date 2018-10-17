c48e635a1a71b7ced187dfea6b0e9d7f69f9120e
praathul <22594358+athulpkr98@users.noreply.github.com>
2018-10-18 00:18:45 +0530

    Gobbler is no more going to starve \n \n   Added food in the playground \n \n   Added scores to calculate the amount of food Gobler had

--- gobbler_source.js
+++ gobbler_source.js
@@ -37,21 +37,40 @@ const changeEaterPosition = function (positionOfEater){
     positionOfEater = positionOfEater + 1;
   }else{
     console.log("Sorry dude , You are dead!!");
+    process.exit();
   }
   return positionOfEater;
 }
 
+const generateFoodPosition = function(){
+  foodPosition = Math.floor(Math.random()*100);
+  return foodPosition;
+}
+
+const printScore = function(score){
+  console.log("Your score is =",score,"!");
+}
+
+
 const main = function(){
   let positionOfEater = 55 ;
+  let foodPosition = generateFoodPosition();
+  let score = 0;
   while(1){
     console.clear();
     let grid=Array(100).fill("   ");
     grid[positionOfEater]="{+}"
+    grid[foodPosition]=" O ";
     array=createPlayground(grid);
     for(line=0;line<12;line++){
       console.log(array[line]);
     }
+    printScore(score);
     positionOfEater = changeEaterPosition(positionOfEater);
+    if(positionOfEater==foodPosition){
+      foodPosition=generateFoodPosition();
+      score += 1;
+    }
   }
 }
 main();
